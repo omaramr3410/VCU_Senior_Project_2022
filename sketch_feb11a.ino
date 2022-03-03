546651a8f21f446a73d798a211250ae6c9d86ebb
omaramr3410 <omaramr3410@gmail.com>
2022-03-03 13:42:49 -0500

    inital commit

---
+++ sketch_feb11a.ino
@@ -0,0 +1,47 @@
+int VRx = A0;
+int VRy = A1;
+int SW = 2;
+
+int xPosition = 0;
+int yPosition = 0;
+int SW_state = 0;
+int mapX = 0;
+int mapY = 0;
+
+void setup() {
+  Serial.begin(9600); 
+  
+  pinMode(VRx, INPUT);
+  pinMode(VRy, INPUT);
+  pinMode(SW, INPUT_PULLUP); 
+  
+  pinMode(13, OUTPUT);    // sets the digital pin 13 as output
+
+  
+}
+
+void loop() {
+  xPosition = analogRead(VRx);
+  yPosition = analogRead(VRy);
+  SW_state = digitalRead(SW);
+  mapX = map(xPosition, 0, 1023, -512, 512);
+  mapY = map(yPosition, 0, 1023, -512, 512);
+  
+  Serial.print("X: ");
+  Serial.print(mapX);
+  Serial.print(" | Y: ");
+  Serial.print(mapY);
+  Serial.print(" | Button: ");
+  Serial.println(SW_state);
+
+  if(mapX > 0){
+    digitalWrite(13, HIGH); // sets the digital pin 13 on
+    delay(1000);   
+  }
+  if(mapY > 0){
+    digitalWrite(13, LOW);  // sets the digital pin 13 off
+    delay(1000);            // waits for a second  
+  }
+  delay(100);
+  
+}

dc8275f1141a4fe96dd67ff8eba105c93a2e28e7
omaramr3410 <omaramr3410@gmail.com>
2022-04-25 19:16:00 -0400

    pushing latest code, contains seperate manual killswitch code

--- wheelchair_system/wheelchair_system.ino
+++ wheelchair_system/wheelchair_system.ino
@@ -25,7 +25,7 @@
  * - This value limits the maximum speed of the motors. 
  * - 255 is the maximum possible value!
  */
-#define MAX_SPEED 255
+#define MAX_SPEED 190
 
 /*
  * MIN_SPEED
@@ -82,6 +82,71 @@ const byte interruptPin_wired = 3;//digital pin input from manual killswitch, a 
 volatile byte state = LOW;//defaults to LOW, thus when powered on, initially can not drive
 volatile int a = 0, b = 0;//a <= pin 2, b <= pin 3
 
+#define LED                         LED_BUILTIN  // digital pin connected to LED, for testing of switch code only
+bool    led_status =                        LOW; // start with LED off, for testing of switch code only
+
+int     button_switch =                       3; // external interrupt pin
+
+#define switched                            true // value if the button switch has been pressed
+#define triggered                           true // controls interrupt handler
+#define interrupt_trigger_type            RISING // interrupt triggered on a RISING input
+#define debounce                              10 // time to wait in milli secs
+
+volatile  bool interrupt_process_status = {
+  !triggered                                     // start with no switch press pending, ie false (!triggered)
+};
+bool initialisation_complete =            false; // inhibit any interrupts until initialisation is complete
+
+//
+// ISR for handling interrupt triggers arising from associated button switch
+//
+void button_interrupt_handler()
+{
+  if (initialisation_complete == true)
+  { //  all variables are initialised so we are okay to continue to process this interrupt
+    if (interrupt_process_status == !triggered) {
+      // new interrupt so okay start a new button read process -
+      // now need to wait for button release plus debounce period to elapse
+      // this will be done in the button_read function
+      if (digitalRead(button_switch) == HIGH) {
+        // button pressed, so we can start the read on/off + debounce cycle wich will
+        // be completed by the button_read() function.
+        interrupt_process_status = triggered;  // keep this ISR 'quiet' until button read fully completed
+      }
+    }
+  }
+} // end of button_interrupt_handler
+
+bool read_button() {
+  int button_reading;
+  // static variables because we need to retain old values between function calls
+  static bool     switching_pending = false;
+  static long int elapse_timer;
+  if (interrupt_process_status == triggered) {
+    // interrupt has been raised on this button so now need to complete
+    // the button read process, ie wait until it has been released
+    // and debounce time elapsed
+    button_reading = digitalRead(button_switch);
+    if (button_reading == HIGH) {
+      // switch is pressed, so start/restart wait for button relealse, plus end of debounce process
+      switching_pending = true;
+      elapse_timer = millis(); // start elapse timing for debounce checking
+    }
+    if (switching_pending && button_reading == LOW) {
+      // switch was pressed, now released, so check if debounce time elapsed
+      if (millis() - elapse_timer >= debounce) {
+        // dounce time elapsed, so switch press cycle complete
+        switching_pending = false;             // reset for next button press interrupt cycle
+        interrupt_process_status = !triggered; // reopen ISR for business now button on/off/debounce cycle complete
+        return switched;                       // advise that switch has been pressed
+      }
+    }
+  }
+  return !switched; // either no press request or debounce period not elapsed
+} // end of read_button function
+
+
+
 //called once at startup
 void setup() {
   // Setup Motor 1 (LEFT MOTOR)
@@ -111,20 +176,41 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(interruptPin_wireless), killswitch, CHANGE);//sensitive to any change to input
   
   //setup dig pin 3 as input
-  pinMode(interruptPin_wired, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin_wired), killswitch, RISING);//sensitive to only rising edge, from 0 to 1
+//  pinMode(interruptPin_wired, INPUT_PULLUP);
+//  attachInterrupt(digitalPinToInterrupt(interruptPin_wired), killswitch2, CHANGE);//sensitive to only rising edge, from 0 to 1
+
+  pinMode(LED, OUTPUT);
+  pinMode(button_switch, INPUT);
+  attachInterrupt(digitalPinToInterrupt(button_switch),
+                  button_interrupt_handler,
+                  interrupt_trigger_type);
+  initialisation_complete = true; // open interrupt processing for business
+  
+
+  Serial.begin(9600);
+  
 }
 
 //called repeatedly 
 void loop() {
   //set output ledPin to state of wheels
   digitalWrite(ledPin, state);
+
+  // test manual killswitch and process if pressed
+  if (read_button() == switched) {
+    // button on/off cycle now complete, so flip LED between HIGH and LOW
+    led_status = HIGH - led_status; // toggle state
+    digitalWrite(LED, led_status);
+    Serial.print("dig pin 2: ");
+  } 
+  
   //check if wheels are off
-  if(state == LOW){
-	motorGo(MOTOR_1, BRAKE, 0);
+  if(state == LOW || led_status == LOW){
+    motorGo(MOTOR_1, BRAKE, 0);
     motorGo(MOTOR_2, BRAKE, 0);
-	//restart loop call
-	return;
+    
+    //restart loop call
+    return;
   }
 
   digitalWrite(EN_PIN_1, HIGH);
@@ -164,6 +250,20 @@ void loop() {
  * - interrupt called on change of Digital Pin 2 or rising edge of Digital Pin 3
  */
 void killswitch() {
+  state = !state;
+  a = digitalRead(2);
+  Serial.print("dig pin 2: ");
+  Serial.print(a);
+  Serial.print("\n\n\n\n\n\n\n\n\n ");
+
+  b = digitalRead(3);
+  Serial.print("dig pin 3 (manual killswitch): ");
+  Serial.print(b);
+  Serial.print("\n\n\n\n\n\n\n\n\n ");
+}
+
+
+void killswitch2() {
   state = !state;
   a = digitalRead(2);
   Serial.print("dig pin 2: ");
